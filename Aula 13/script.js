104a5c122043cb5ba108c15603c34e894ac1b05b
xscoutt <wesleyoliveira4748@gmail.com>
2022-11-10 20:37:02 +0000

    updatecurso

---
+++ Aula 13/script.js
@@ -0,0 +1,25 @@
+function carregar (){
+var msg = window.document.getElementById('msg')
+var imagem = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+//var minuto = data.getMinutes()
+msg.innerHTML = `Agora são<strong> ${hora} horas<strong>.`
+if (hora >= 0 && hora < 12) {
+     //BOM DIA
+   imagem.src = 'manha.png'
+   DocumentTimeline.body.style.background = '#e2cd9f'
+  //
+} else if (hora >= 12 && hora< 18) {
+    //BOA TARDE!
+    imagem.src = 'tarde.png'
+    document.body.style.background = '#b9846f'
+
+} else {
+    //BOA NOITE!
+    imagem.src = 'noite.png'
+    document.body.style.background = '#515154'
+   
+} 
+}
+carregar()
